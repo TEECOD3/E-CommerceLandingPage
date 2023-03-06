13ab7d296416f24e659c25de9f14591fb2b58bd6
david temitope <temi236topemicheal@gmail.com>
2023-03-06 01:45:37 +0100

    initital commit

---
+++ src/Icons/IconNext.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+const NextIcon = (props) => (
+  <svg width={13} height={18} xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="m2 1 8 8-8 8"
+      stroke="#1D2026"
+      strokeWidth={3}
+      fill="none"
+      fillRule="evenodd"
+    />
+  </svg>
+);
+export default NextIcon;
