6078463e9b969c9b0bf70e0e34e71b6754aefc00
david temitope <temi236topemicheal@gmail.com>
2023-03-07 15:53:16 +0100

    new branch for card✨

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Navbar from "./Components/UI/Navbar";
+import { useState } from "react";
+import imagedata from "./Data/Data";
 
 function App() {
   return (
     <>
       <Navbar />
+      <main className="bg-red-500 h-[80vh] ">
+        <div className="bg-yellow-400 h-[70vh] w-[80%] max-sm:w-full mx-auto mt-4">
+          {/* {imagedata.map((image) => (
+            <img src={image.mainImage} />
+          ))} */}
+        </div>
+      </main>
     </>
   );
 }
